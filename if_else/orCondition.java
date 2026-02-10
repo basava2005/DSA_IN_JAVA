df2f11e942ff30be80b96100d93b66ce040605a1
basava2005 <basavarajha05@gmail.com>
2026-02-10 21:48:36 +0530

    feb10_divisible by 5 0r 3

---
+++ if_else/orCondition.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class orCondition {
+    
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+        int n= sc.nextInt();
+
+        if(n%5==0 || n%3==0){
+            System.out.println("the number is divisible by 5 or 3   ");
+
+        }else{
+            System.out.println("not divisible by 5 or 3");
+
+        }
+        sc.close();
+    }
+}
+
+
